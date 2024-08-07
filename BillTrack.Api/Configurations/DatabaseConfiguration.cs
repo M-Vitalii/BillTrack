221c52d3f595257d9579af7ffbe1fb9f898e14fe
M-Vitalii <vitalii.merdak@tech-stack.io>
2024-08-07 16:34:31 +0300

    Add database configuration extension

---
+++ BillTrack.Api/Configurations/DatabaseConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using BillTrack.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace BillTrack.Api.Configurations;
+
+public static class DatabaseConfiguration
+{
+    public static IServiceCollection ConfigureDatabase(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+        return services;
+    }
+}
+
--- BillTrack.Api/Program.cs
+++ BillTrack.Api/Program.cs
@@ -1,8 +1,11 @@
+using BillTrack.Api.Configurations;
 using FastEndpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddFastEndpoints();
+
+builder.Services.ConfigureDatabase(builder.Configuration);
 
 var app = builder.Build();
 
