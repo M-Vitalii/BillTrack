39db7e37c236c5935b298d45682cd6b89bed5548
M-Vitalii <vitalii.merdak@tech-stack.io>
2024-08-09 14:42:05 +0300

    Change hours type from TimeOnly to decimal

--- BillTrack.Domain/Entities/Workday.cs
+++ BillTrack.Domain/Entities/Workday.cs
@@ -3,7 +3,7 @@ namespace BillTrack.Domain.Entities;
 public class Workday : AuditableEntity
 {
     public DateOnly Date { get; set; }
-    public TimeOnly Hours { get; set; }
+    public decimal Hours { get; set; }
     public Guid EmployeeId { get; set; }
     
     public required Employee Employee { get; set; }
