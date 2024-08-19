be702644eef528b1114ca1c66fa9c086cc48e886
M-Vitalii <vitalii.merdak@tech-stack.io>
2024-08-19 16:32:17 +0300

    Add Web api service

---
+++ BillTrack.Application/Services/WebApiService.cs
@@ -0,0 +1,70 @@
+using BillTrack.Core.Interfaces.Repositories;
+using BillTrack.Domain.Entities;
+
+namespace BillTrack.Application.Services;
+
+public class WebApiService
+{
+    private readonly Dictionary<Type, object> _repositories;
+
+    public WebApiService(
+        IGenericRepository<Project> projectRepository,
+        IGenericRepository<Department> departmentRepository,
+        IGenericRepository<Invoice> invoiceRepository,
+        IGenericRepository<Workday> workdayRepository,
+        IGenericRepository<Employee> employeeRepository)
+    {
+        _repositories = new Dictionary<Type, object>
+        {
+            { typeof(Project), projectRepository },
+            { typeof(Department), departmentRepository },
+            { typeof(Invoice), invoiceRepository },
+            { typeof(Workday), workdayRepository },
+            { typeof(Employee), employeeRepository },
+        };
+    }
+    
+    private IGenericRepository<T> GetRepository<T>() where T : AuditableEntity
+    {
+        if (_repositories.TryGetValue(typeof(T), out var repository))
+        { 
+            return (IGenericRepository<T>)repository;
+        }
+
+        throw new InvalidOperationException($"Repository for type {typeof(T)} not found.");
+    }
+    
+    public async Task<T> CreateAsync<T>(T entity) where T : AuditableEntity
+    {
+        return await GetRepository<T>().AddAsync(entity);
+    }
+
+    public async Task<T?> GetByIdAsync<T>(Guid id) where T : AuditableEntity
+    {
+        return await GetRepository<T>().GetByIdAsync(id);
+    }
+
+    public IQueryable<T> GetAll<T>() where T : AuditableEntity
+    {
+        return GetRepository<T>().GetAllAsync();
+    }
+
+    public async Task UpdateAsync<T>(Guid id, T entity) where T : AuditableEntity
+    {
+        await GetRepository<T>().UpdateAsync(entity);
+    }
+
+    public async Task DeleteAsync<T>(Guid id) where T : AuditableEntity
+    {
+        IGenericRepository<T> repository = GetRepository<T>();
+    
+        var entity = await repository.GetByIdAsync(id);
+        
+        if (entity == null)
+        {
+            throw new InvalidOperationException($"Entity of type {typeof(T)} with ID {id} not found.");
+        }
+    
+        await repository.DeleteAsync(entity);
+    }
+}
