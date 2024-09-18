09f80f4bd0066b45b3dd389a0d581abf5796cae9
M-Vitalii <vitalii.merdak@tech-stack.io>
2024-09-18 18:35:31 +0300

    Add Structured logging using Serilog to lambda function

--- BillTrack.Worker/Function.cs
+++ BillTrack.Worker/Function.cs
@@ -1,4 +1,6 @@
+using System.Reflection;
 using System.Text.Json;
+using Amazon;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.Serialization.SystemTextJson;
 using Amazon.Lambda.SQSEvents;
@@ -11,7 +13,12 @@ using BillTrack.Worker.Configurations;
 using BillTrack.Worker.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Serilog;
+using Serilog.Formatting.Json;
+using Serilog.Sinks.AwsCloudWatch;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -33,46 +40,50 @@ public class Function
         ConfigureServices(serviceCollection);
         var serviceProvider = serviceCollection.BuildServiceProvider();
 
-        _fileGenerator = serviceProvider.GetService<IFileGenerator>()!;
-        _fileUploader = serviceProvider.GetService<IFileUploader>()!;
-        _invoiceRepository = serviceProvider.GetService<IGenericRepository<Invoice>>()!;
+        _fileGenerator = serviceProvider.GetRequiredService<IFileGenerator>();
+        _fileUploader = serviceProvider.GetRequiredService<IFileUploader>();
+        _invoiceRepository = serviceProvider.GetRequiredService<IGenericRepository<Invoice>>();
         _appConfiguration = serviceProvider.GetRequiredService<IOptions<AppConfiguration>>().Value;
     }
 
-    public async Task FunctionHandler(SQSEvent evnt, ILambdaContext context)
+    public async Task FunctionHandler(SQSEvent evnt)
     {
         foreach (var message in evnt.Records)
         {
-            await ProcessMessageAsync(message, context);
+            await ProcessMessageAsync(message);
         }
     }
 
-    private async Task ProcessMessageAsync(
-        SQSEvent.SQSMessage message,
-        ILambdaContext context)
+    private async Task ProcessMessageAsync(SQSEvent.SQSMessage message)
     {
-        var logger = context.Logger;
         var invoice = JsonSerializer.Deserialize<CreatedInvoice>(message.Body);
-        
+
         var fileName = $"invoice-{invoice.InvoiceId}.pdf";
-        
+
         try
         {
             var pdfStream = await _fileGenerator.GenerateFile(invoice.InvoiceId);
 
             try
             {
-                await _fileUploader.UploadFileToS3(pdfStream, _appConfiguration.BucketName, fileName, invoice.InvoiceId);
+                await _fileUploader.UploadFileToS3(pdfStream, _appConfiguration.BucketName, fileName,
+                    invoice.InvoiceId);
             }
             catch (Exception ex)
             {
-                logger.LogError($"Failed to upload PDF for InvoiceId: {invoice.InvoiceId}");
+                Log.Logger.Error(
+                    ex,
+                    "Failed to upload PDF for InvoiceId: {InvoiceId} with sqs message: {@SqsMessage}",
+                    invoice.InvoiceId, message);
                 throw;
             }
         }
         catch (Exception ex)
         {
-            logger.LogError($"Failed to generate PDF for InvoiceId: {invoice.InvoiceId}");
+            Log.Logger.Error(
+                ex,
+                "Failed to generate PDF for InvoiceId: {InvoiceId} with sqs message: {@SqsMessage}",
+                invoice.InvoiceId, message);
             throw;
         }
 
@@ -80,29 +91,43 @@ public class Function
 
         await Task.CompletedTask;
     }
-    
-        
+
+
     private async Task UpdateInvoiceUrl(Guid invoiceId, string fileName)
     {
         var invoice = await _invoiceRepository.GetByIdAsync(invoiceId);
 
-        invoice.InvoiceUrl = $"https://{_appConfiguration.BucketName}.s3.{_appConfiguration.AwsRegion}.amazonaws.com/{fileName}";
-        
+        invoice.InvoiceUrl =
+            $"https://{_appConfiguration.BucketName}.s3.{_appConfiguration.AwsRegion}.amazonaws.com/{fileName}";
+
         await _invoiceRepository.UpdateAsync(invoice);
     }
 
     private void ConfigureServices(IServiceCollection services)
     {
         var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile($"appsettings.json")
             .AddEnvironmentVariables()
             .Build();
-        
-        services.Configure<AppConfiguration>(configuration.Bind);
-        
+
+        services.Configure<AppConfiguration>(config =>
+        {
+            config.BucketName = configuration["BUCKET_NAME"];
+            config.AwsRegion = configuration["AWS_REGION"];
+            config.ConnectionString = configuration["CONNECTION_STRING"];
+        });
+
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .WriteTo.Console(new JsonFormatter())
+            .CreateLogger();
+
         services
             .ConfigureDatabase(configuration["CONNECTION_STRING"])
             .ConfigureRepositories();
 
+        services.AddTransient<IEmployeeSalaryCalculator, EmployeeSalaryCalculator>();
         services.AddTransient<IFileGenerator, InvoicePdfGenerator>();
         services.AddTransient<IFileUploader, InvoicePdfUploader>();
 
