2b35c66bc65477fd39d794ec6507674eff68bf8f
M-Vitalii <vitalii.merdak@tech-stack.io>
2024-09-20 16:02:52 +0300

    Add IMessage implementation in CreatedInvoice message model

--- BillTrack.Core/Contracts/SqsMessages/CreatedInvoice.cs
+++ BillTrack.Core/Contracts/SqsMessages/CreatedInvoice.cs
@@ -1,9 +1,12 @@
 using System.Text.Json.Serialization;
+using BillTrack.Core.Interfaces.Models;
 
 namespace BillTrack.Core.Contracts.SqsMessages;
 
-public class CreatedInvoice
+public class CreatedInvoice : IMessage
 {
     [JsonPropertyName("invoiceId")]
     public Guid InvoiceId { get; set; }
+
+    public string MessageType => nameof(CreatedInvoice);
 }
